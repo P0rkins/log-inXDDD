b0bcca2806ff1b5bc8687231a02013a6754f65d4
Lorenz Vargas <Lorenz Vargas>
2016-10-05 14:47:36 -0400

    fixed dumb errors

--- hap.py
+++ hap.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import random
+import csv
+import hashlib
 app = Flask(__name__)
 
 
@@ -8,17 +10,42 @@ def login():
     print request.headers          #only works for POST
     return render_template( 'login.html' )
 
+@app.route("/registration/", methods["POST","GET"])
+def register():
+    return render_template('register.html', utitle = "Register")
 
-@app.route("/auth", methods=['POST','GET'])
+@app.route("/regauth/", methods=["POST", "GET"])
+def regauth():
+	form = request.form;
+	user = hashlib.sha1(form['user']).hexdigest()
+	password = hashlib.sha1(form['password']).hexdigest()
+	with open('data/accounts.csv', 'rb') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			if (user == row[0]):
+				return render_template('register.html', message = 'Username already registered!' , title = 'Register')
+	f.close()
+
+	fd = open('data/accounts.csv','a')
+	fd.write(user + ',' + password)
+	fd.close()
+	return render_template('register.html', message = 'Account Registered!' , title = 'Register')
+
+
+@app.route("/authenticate", methods=['POST','GET'])
 def authenticate():
-    username = "P0rkins"
-    password = "red6"
     inputUser = request.form['username']
     inputPass = request.form['password']
-    if username == inputUser and password == inputPass:
-        return render_template('authenticate.html', utitle = "Death Star destroyed", flag = 1)
-    else:
-        return render_template('authenticate.html', utitle = "RIP Porkins", flag = 0)
+    user = hashlib.sha1(inputUser).hexdigest()
+    pw = hashlib.sha1(inputPass).hexdigest()
+    with open('data/accounts.csv','rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if (user == row[0] and pw == row[1]):
+                f.close()
+                return render_template('authenticate.html', utitle = "Death Star destroyed", flag = 1)
+    f.close()
+    return render_template('authenticate.html', utitle = "RIP Porkins", flag = 0)
 
     
 if __name__ == "__main__": #false if this file imported as module
