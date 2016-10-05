f82a9715440c129930074fdbe59308050b26b827
Laurentius912 <lvargas@stuy.edu>
2016-10-04 22:30:01 -0400

    done

--- hap.py
+++ hap.py
@@ -1,32 +1,24 @@
 from flask import Flask, render_template, request
-
+import random
 app = Flask(__name__)
 
 
 @app.route("/")
 def login():
-    print app
-    print request
-    print request.args
-    #print request.args['username'] #if username submitted
-    #print request.headers          #only works for POST
+    print request.headers          #only works for POST
     return render_template( 'login.html' )
 
 
-@app.route("/auth", methods=['POST'])
+@app.route("/auth", methods=['POST','GET'])
 def authenticate():
     username = "P0rkins"
     password = "red6"
     inputUser = request.form['username']
     inputPass = request.form['password']
     if username == inputUser and password == inputPass:
-        utitle = "You can hold it!"
-        utext = "Death Star Destroyed."
+        return render_template('authenticate.html', utitle = "Death Star destroyed", flag = 1)
     else:
-        utitle = "Ahhhh"
-        utext = "RIP Porkins"
-
-    return render_template("success.html", title = utitle, text = utext)
+        return render_template('authenticate.html', utitle = "RIP Porkins", flag = 0)
 
     
 if __name__ == "__main__": #false if this file imported as module
